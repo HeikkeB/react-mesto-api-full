869397d06e153976a87f51f654b8859b8ef22c61
HeikkeB <vldbltn@gmail.com>
2022-12-22 01:57:54 +0300

    feat: make api

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -91,7 +91,7 @@ class Api {
 }
 
 export const api = new Api({
-  baseUrl: 'http://api.mymesto.nomoredomains.club',
+  baseUrl: 'http://myMesto.nomoredomains.club',
   headers: {
     authorization: `${AUTH}`,
     'Content-Type': 'application/json',
