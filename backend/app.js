f4c9bb1ed2d33beca1d3547f93edf636d68288f7
HeikkeB <vldbltn@gmail.com>
2022-12-22 16:36:40 +0300

    feat: make crash

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const { limiter } = require('./middlewares/limiter');
 const auth = require('./middlewares/auth');
 const { handleErrors } = require('./middlewares/handleErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const routerCrash = require('./errors/CrashTest');
+// const routerCrash = require('./errors/CrashTest');
 
 const { PORT = 3000 } = process.env;
 
@@ -29,7 +29,11 @@ app.use(limiter);
 app.use(requestLogger);
 
 // crash test
-app.use(routerCrash);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Attention! The server is about to crash');
+  }, 0);
+});
 // routes
 app.use(routerAuth);
 app.use(auth, router);
