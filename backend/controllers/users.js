85b70f8258e2c9b546d55571ea4a10d9f44c8f55
HeikkeB <vldbltn@gmail.com>
2022-12-24 18:14:51 +0300

    fix

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -107,6 +107,7 @@ module.exports.login = (req, res, next) => {
       //   httpOnly: true,
       //   sameSite: true,
       // });
+
       res.send({ message: 'Authorization was successful!', token });
     })
     .catch((err) => {
