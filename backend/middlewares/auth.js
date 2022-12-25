b077dc75c3dbb655271c411e5160bbce1a216064
HeikkeB <vldbltn@gmail.com>
2022-12-25 14:13:40 +0300

    fix 401

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,21 +4,38 @@ const Unauthorized = require('../errors/Unauthorized');
 
 const { JWT_SECRET = 'fc940a6da342577ec7ecc725c90a5037' } = process.env;
 
+// module.exports = (req, res, next) => {
+//   const { authorization } = req.headers;
+//   if (!authorization || !authorization.startsWith('Bearer ')) {
+//     return next(new Unauthorized('Authorization required'));
+//   }
+
+//   const token = authorization.replace('Bearer ', '');
+//   let payload;
+
+//   try {
+//     payload = jwt.verify(token, JWT_SECRET);
+//   } catch (error) {
+//     return next(new Unauthorized('Authorization required!'));
+//   }
+
+//   req.user = payload;
+//   return next();
+// };
+
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new Unauthorized('Authorization required'));
+  if (!req.cookies.jwt) {
+    next(new Unauthorized('Authorization required'));
+  } else {
+    const token = req.cookies.jwt;
+    let payload;
+
+    try {
+      payload = jwt.verify(token, JWT_SECRET);
+      req.user = payload;
+      next();
+    } catch (err) {
+      next(new Unauthorized('Authorization required'));
+    }
   }
-
-  const token = authorization.replace('Bearer ', '');
-  let payload;
-
-  try {
-    payload = jwt.verify(token, JWT_SECRET);
-  } catch (error) {
-    return next(new Unauthorized('Authorization required!'));
-  }
-
-  req.user = payload;
-  return next();
 };
